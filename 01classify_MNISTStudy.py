76f3f9bce7cb07a4b8224ef6707de0ece4a3c99d
Daniele Ancora, Ph.D <38218284+danieleancora@users.noreply.github.com>
2024-04-05 15:28:49 +0200

    Add files via upload

---
+++ 01classify_MNISTStudy.py
@@ -0,0 +1,424 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  9 15:58:09 2022
+
+This code perform a study on classification performance of various output
+datasets originated by a given MNIST input set.
+
+It includes classification results from:
+    - standard MNIST
+    - zoomed MNIST
+    - randomized MNIST
+    - simulated speckles output with measured TM and MNIST as input
+    - measured speckles output after propagation through the MMF
+
+!!! WARNING: This code works with very big matrices and may take a very long 
+time to execute, eventually terminating hardware memory and crashing. 
+The code was tested with powerful workstation equipped with 128Gb of RAM.
+
+@author: Daniele Ancora
+"""
+
+import cupy as cp
+import numpy as np
+import matplotlib.pyplot as plt
+from cuml.linear_model import LogisticRegression
+from scipy.ndimage import zoom
+
+
+# %% LOAD THE FULL DATASET
+foldername = ''
+camera = 'Alpha'
+
+fname = 'MNISTSet00' + '_10000_P28_handwritten_inA_cam_' + camera + '.npz'
+
+# pointer to the compressed object
+dataset = np.load(foldername + fname)
+
+# dataset is a dictionary, let us define appropriate pointers
+labels  = dataset['arr_0']
+slm     = dataset['arr_1']
+
+# load also the measured speckle patterns
+speckleA = dataset['arr_2']
+speckleA = speckleA[:,150:-150,150:-150]
+
+
+# %% SET GLOBAL PARAMETERS OF THE STUDY
+mu = 4800
+
+C = 64             # regularization term
+P = mu-1000        # maximum number of measurements used for train
+stepP = 500        # increase measurement train number by this amount
+
+attempts = 100           # number of random initializations of the classification
+runfor = int(P/stepP)
+
+# dimension of the patterns used for training, it has to be the same for the MMF used
+targetsize = 600
+
+
+
+
+
+# %% ######################################################## CLASSIFICATION 01
+# MNIST normal resolution ------------------------------------- RUN THIS ONE!!!
+X = cp.asarray(slm[:mu,:,:], dtype=cp.float32)
+Y = cp.asarray(labels[:mu]) 
+
+X = X.reshape(X.shape[0],X.shape[1]*X.shape[2])
+X = X / X.max()
+
+
+store_train_acc = np.zeros((attempts, runfor))
+store_test_acc = np.zeros((attempts, runfor))
+store_howmany = np.zeros(runfor)
+
+for i in range(attempts):
+    for j in range(runfor):
+
+        # SHUFFLE DATASET CONSISTENTLY
+        seed = np.random.randint(0,2**32-1)
+        cp.random.seed(seed)
+        cp.random.shuffle(X)
+        cp.random.seed(seed)
+        cp.random.shuffle(Y)
+
+        howmany = stepP*(j+1)
+        store_howmany[j] = howmany
+        print('(',i,',', j, ')', 'train with = ', howmany, 'measurements')
+
+        # split dataset
+        train_X = X[:howmany,:]
+        train_Y = Y[:howmany]
+        test_X = X[P:,:]
+        test_Y = Y[P:]
+    
+        # TRAIN THE LINEAR MODEL
+        model = LogisticRegression(solver="qn", max_iter=1e4, tol=1e-5, penalty="l2", C=C, verbose=2).fit(train_X,train_Y)        
+    
+        # VERIFY THE MODEL SCORE
+        train_acc = model.score(train_X,train_Y)
+        test_acc = model.score(test_X,test_Y)
+        
+        store_train_acc[i,j] = train_acc 
+        store_test_acc[i,j] = test_acc 
+        
+        print('train accuracy = ',train_acc, 'test accuracy = ', test_acc)
+
+
+# %% SAVE STUFF
+fnamesave = 'ACCURACY_train' + str(P) + 'test'+str(1000)+'_mnist'
+cp.savez(fnamesave, store_howmany, store_train_acc, store_test_acc)
+
+opendata = np.load(fnamesave+'.npz')
+store_howmany_MNISTstandard = opendata['arr_0']
+store_train_acc_MNISTstandard = opendata['arr_1']
+store_test_acc_MNISTstandard = opendata['arr_2']
+
+plt.plot(store_test_acc_MNISTstandard.mean(axis=0))
+
+
+
+
+
+
+# %% ######################################################## CLASSIFICATION 02
+# MNIST normal distribution random real transmission ---------- RUN THIS ONE!!!
+X = np.asarray(slm[:mu,:,:], dtype=np.float32)
+
+transmission = np.abs(np.random.randn(784,targetsize*targetsize))
+X = X.reshape(X.shape[0],X.shape[1]*X.shape[2])
+X = np.matmul(X,transmission)
+
+X = np.asarray(X, dtype=np.float32)
+X = X / X.max()
+
+Y = np.asarray(labels[:mu]) 
+
+
+# %% start the classification
+store_train_acc = np.zeros((attempts, runfor))
+store_test_acc = np.zeros((attempts, runfor))
+store_howmany = np.zeros(runfor)
+
+for i in range(attempts):
+    for j in range(runfor):
+
+        # SHUFFLE DATASET CONSISTENTLY
+        seed = np.random.randint(0,2**32-1)
+        np.random.seed(seed)
+        np.random.shuffle(X)
+        np.random.seed(seed)
+        np.random.shuffle(Y)
+
+        howmany = stepP*(j+1)
+        store_howmany[j] = howmany
+        print('(',i,',', j, ')', 'train with = ', howmany, 'measurements')
+
+        # split dataset
+        train_X = X[:howmany,:]
+        train_Y = Y[:howmany]
+        test_X = X[P:,:]
+        test_Y = Y[P:]
+    
+        # TRAIN THE LINEAR MODEL
+        model = LogisticRegression(solver="qn", max_iter=1e4, tol=1e-5, penalty="l2", C=C, verbose=2).fit(train_X,train_Y)        
+    
+        # VERIFY THE MODEL SCORE
+        train_acc = model.score(train_X,train_Y)
+        test_acc = model.score(test_X,test_Y)
+        
+        store_train_acc[i,j] = train_acc 
+        store_test_acc[i,j] = test_acc 
+        
+        print('train accuracy = ',train_acc, 'test accuracy = ', test_acc)
+
+
+# %% SAVE STUFF
+fnamesave = 'ACCURACY_train' + str(P) + 'test'+str(1000)+'_mnistRandom'
+cp.savez(fnamesave, store_howmany,store_train_acc,store_test_acc)
+
+opendata = np.load(fnamesave+'.npz')
+store_howmany_MNISTrandom = opendata['arr_0']
+store_train_acc_MNISTrandom = opendata['arr_1']
+store_test_acc_MNISTrandom = opendata['arr_2']
+
+plt.plot(store_test_acc_MNISTrandom.mean(axis=0))
+
+
+
+
+
+
+
+
+# %% ######################################################## CLASSIFICATION 03
+# MNIST zoomed dataset using linear interpolation ------------ RUN THIS ONE!!!!
+X = np.asarray(slm[:mu,:,:], dtype=cp.float32)
+X = zoom(X, (1, targetsize/slm.shape[1], targetsize/slm.shape[2]), order=1)
+X = X.reshape(X.shape[0],X.shape[1]*X.shape[2])
+X = X / X.max()
+
+X = np.asarray(X, dtype=cp.float32)
+
+Y = np.asarray(labels[:mu])
+
+
+# %% start the classification
+store_train_acc = np.zeros((attempts, runfor))
+store_test_acc = np.zeros((attempts, runfor))
+store_howmany = np.zeros(runfor)
+
+for i in range(attempts):
+    for j in range(runfor):
+
+        # SHUFFLE DATASET CONSISTENTLY
+        seed = np.random.randint(0,2**32-1)
+        np.random.seed(seed)
+        np.random.shuffle(X)
+        np.random.seed(seed)
+        np.random.shuffle(Y)
+
+        howmany = stepP*(j+1)
+        store_howmany[j] = howmany
+        print('(',i,',', j, ')', 'train with = ', howmany, 'measurements')
+
+        # split dataset
+        train_X = X[:howmany,:]
+        train_Y = Y[:howmany]
+        test_X = X[P:,:]
+        test_Y = Y[P:]
+    
+        # TRAIN THE LINEAR MODEL
+        model = LogisticRegression(solver="qn", max_iter=1e4, tol=1e-5, penalty="l2", C=C, verbose=2).fit(train_X,train_Y)        
+    
+        # VERIFY THE MODEL SCORE
+        train_acc = model.score(train_X,train_Y)
+        test_acc = model.score(test_X,test_Y)
+        
+        store_train_acc[i,j] = train_acc 
+        store_test_acc[i,j] = test_acc 
+        
+        print('train accuracy = ',train_acc, 'test accuracy = ', test_acc)
+
+
+# %% SAVE STUFF
+fnamesave = 'ACCURACY_train' + str(P) + 'test' + str(1000) + '_mnistZoom'
+cp.savez(fnamesave, store_howmany, store_train_acc, store_test_acc)
+
+opendata = np.load(fnamesave+'.npz')
+store_howmany_MNISTzoom = opendata['arr_0']
+store_train_acc_MNISTzoom = opendata['arr_1']
+store_test_acc_MNISTzoom = opendata['arr_2']
+
+plt.plot(store_test_acc_MNISTzoom.mean(axis=0))
+
+
+
+
+
+
+# %% ######################################################## CLASSIFICATION 04
+# MNIST passing through estimated TM, speckle simulation ----- RUN THIS ONE!!!!
+fname = 'TM00' + "_10000_P28_handwritten_inA_cam_Alpha"
+tmXY = np.load(fname+'.npz')
+tmXY = tmXY['arr_0']
+
+# the entire tmXY leads to output size of 900x900 which we crop down to 600x600
+outputL = int((tmXY.shape[1])**0.5)
+tmXY = tmXY.reshape(tmXY.shape[0], outputL, outputL)
+tmXY = tmXY[:,150:-150,150:-150]
+tmXY = tmXY.reshape(tmXY.shape[0], tmXY.shape[1]*tmXY.shape[2])
+
+# apply the trasmission matrix to the slm input to obtain the speckle output
+X = np.asarray(slm, dtype=cp.float32)
+X = X.reshape(X.shape[0],X.shape[1]*X.shape[2])
+X = np.matmul(X,tmXY)
+X = np.abs(X)
+
+del tmXY
+
+X = np.asarray(X, dtype=np.float32)
+X = X / X.max()
+
+Y = np.asarray(labels) 
+
+
+# %% SET PARAMETERS OF THE STUDY
+store_train_acc = np.zeros((attempts, runfor))
+store_test_acc = np.zeros((attempts, runfor))
+store_howmany = np.zeros(runfor)
+
+for i in range(attempts):
+    for j in range(runfor):
+
+        # SHUFFLE DATASET CONSISTENTLY
+        seed = np.random.randint(0,2**32-1)
+        np.random.seed(seed)
+        np.random.shuffle(X)
+        np.random.seed(seed)
+        np.random.shuffle(Y)
+
+        howmany = stepP*(j+1)
+        store_howmany[j] = howmany
+        print('(',i,',', j, ')', 'train with = ', howmany, 'measurements')
+
+        # split dataset
+        train_X = X[:howmany,:]
+        train_Y = Y[:howmany]
+        test_X = X[P:,:]
+        test_Y = Y[P:]
+    
+        # TRAIN THE LINEAR MODEL
+        model = LogisticRegression(solver="qn", max_iter=1e4, tol=1e-5, penalty="l2", C=C, verbose=2).fit(train_X,train_Y)        
+    
+        # VERIFY THE MODEL SCORE
+        train_acc = model.score(train_X,train_Y)
+        test_acc = model.score(test_X,test_Y)
+        
+        store_train_acc[i,j] = train_acc 
+        store_test_acc[i,j] = test_acc 
+        
+        print('train accuracy = ',train_acc, 'test accuracy = ', test_acc)
+
+
+# %% SAVE STUFF
+fnamesave = 'ACCURACY_train' + str(P) + 'test'+str(1000)+'_specklSimula'
+cp.savez(fnamesave, store_howmany,store_train_acc,store_test_acc)
+
+opendata = np.load(fnamesave+'.npz')
+store_howmany_SPECKLEsimul = opendata['arr_0']
+store_train_acc_SPECKLEsimul = opendata['arr_1']
+store_test_acc_SPECKLEsimul = opendata['arr_2']
+
+
+plt.plot(store_test_acc_SPECKLEsimul.mean(axis=0))
+
+
+
+
+
+
+
+
+# %% ######################################################## CLASSIFICATION 05
+# SPECKLE recorded in camera after mnist propagation --------- RUN THIS ONE!!!!
+X = np.asarray(speckleA[:mu,:,:], dtype=np.float32)
+Y = np.asarray(labels[:mu]) 
+
+X = X.reshape(X.shape[0],X.shape[1]*X.shape[2])
+X = X**0.5
+X = X / X.max()
+
+
+# %% start the classification
+store_howmany = np.zeros(runfor)
+store_train_acc = np.zeros((attempts, runfor))
+store_test_acc = np.zeros((attempts, runfor))
+
+for i in range(attempts):
+    for j in range(runfor):
+
+        # SHUFFLE DATASET CONSISTENTLY
+        seed = np.random.randint(0,2**32-1)
+        np.random.seed(seed)
+        np.random.shuffle(X)
+        np.random.seed(seed)
+        np.random.shuffle(Y)
+
+        howmany = stepP*(j+1)
+        store_howmany[j] = howmany
+        print('(',i,',', j, ')', 'train with = ', howmany, 'measurements')
+
+        # split dataset
+        train_X = X[:howmany,:]
+        train_Y = Y[:howmany]
+        test_X = X[P:,:]
+        test_Y = Y[P:]
+    
+        # TRAIN THE LINEAR MODEL
+        model = LogisticRegression(solver="qn", max_iter=1e4, tol=1e-5, penalty="l2", C=C, verbose=2).fit(train_X,train_Y)        
+    
+        # VERIFY THE MODEL SCORE
+        train_acc = model.score(train_X,train_Y)
+        test_acc = model.score(test_X,test_Y)
+        
+        store_train_acc[i,j] = train_acc 
+        store_test_acc[i,j] = test_acc 
+        
+        print('train accuracy = ', train_acc, 'test accuracy = ', test_acc)
+
+
+# %% SAVE STUFF
+fnamesave = 'ACCURACY_train' + str(P) + 'test'+str(1000)+'_specklMeasured'
+cp.savez(fnamesave, store_howmany,store_train_acc,store_test_acc)
+
+opendata = np.load(fnamesave+'.npz')
+store_howmany_SPECKLEsimul = opendata['arr_0']
+store_train_acc_SPECKLEsimul = opendata['arr_1']
+store_test_acc_SPECKLEsimul = opendata['arr_2']
+
+
+plt.plot(store_test_acc_SPECKLEsimul.mean(axis=0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02plot_resultVisualization.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 14:25:15 2024
+
+@author: daniele
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+P = 3800
+stepP = 500
+
+# %% LOAD EVERYTHING
+# MNIST normal resolution ------------------------------------- RUN THIS ONE!!!
+fnamesave = 'ACCURACY_train' + str(P) + 'test' + str(1000) + '_mnist'
+opendata = np.load(fnamesave+'.npz')
+store_howmany_MNISTstandard = opendata['arr_0']
+store_train_acc_MNISTstandard = opendata['arr_1']
+store_test_acc_MNISTstandard = opendata['arr_2']
+
+
+# MNIST normal distribution random real transmission ---------- RUN THIS ONE!!!
+fnamesave = 'ACCURACY_train' + str(P) + 'test' + str(1000) + '_mnistRandom'
+opendata = np.load(fnamesave+'.npz')
+store_howmany_MNISTrandom = opendata['arr_0']
+store_train_acc_MNISTrandom = opendata['arr_1']
+store_test_acc_MNISTrandom = opendata['arr_2']
+
+
+# MNIST zoomed dataset using linear interpolation ------------ RUN THIS ONE!!!!
+fnamesave = 'ACCURACY_train' + str(P) + 'test' + str(1000) + '_mnistZoom'
+opendata = np.load(fnamesave+'.npz')
+store_howmany_MNISTzoom = opendata['arr_0']
+store_train_acc_MNISTzoom = opendata['arr_1']
+store_test_acc_MNISTzoom = opendata['arr_2']
+
+
+# MNIST passing through estimated TM, speckle simulation ----- RUN THIS ONE!!!!
+fnamesave = 'ACCURACY_train' + str(P) + 'test' + str(1000)+'_specklSimula'
+opendata = np.load(fnamesave+'.npz')
+store_howmany_SPECKLEsimul = opendata['arr_0']
+store_train_acc_SPECKLEsimul = opendata['arr_1']
+store_test_acc_SPECKLEsimul = opendata['arr_2']
+
+
+# SPECKLE recorded in camera after mnist propagation --------- RUN THIS ONE!!!!
+fnamesave = 'ACCURACY_train' + str(P) + 'test' + str(1000)+'_specklMeasured'
+opendata = np.load(fnamesave+'.npz')
+store_howmany_SPECKLEsimul = opendata['arr_0']
+store_train_acc_SPECKLEsimul = opendata['arr_1']
+store_test_acc_SPECKLEsimul = opendata['arr_2']
+
+
+# %% PLOT
+samples = np.arange(1, store_test_acc_MNISTstandard.shape[1]+1)*stepP
+
+plt.errorbar(samples, store_test_acc_MNISTstandard.mean(axis=0), yerr=store_test_acc_MNISTstandard.std(axis=0), label='MNIST original')
+plt.errorbar(samples, store_test_acc_MNISTrandom.mean(axis=0), yerr=store_test_acc_MNISTrandom.std(axis=0), label='MNIST randomized')
+plt.errorbar(samples, store_test_acc_MNISTzoom.mean(axis=0), yerr=store_test_acc_MNISTzoom.std(axis=0), label='MNIST upscaled')
+plt.errorbar(samples, store_test_acc_SPECKLEsimul.mean(axis=0), yerr=store_test_acc_SPECKLEsimul.std(axis=0), label='MMF a-simulated')
+plt.errorbar(samples, store_test_acc_SPECKLEsimul.mean(axis=0), yerr=store_test_acc_SPECKLEsimul.std(axis=0), label='MMF a-measured')
+plt.xlabel('Training samples [M]')
+plt.ylabel('Test accuracy')
+plt.legend()
+
+
+
+
